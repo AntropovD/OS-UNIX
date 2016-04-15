327efe99a958a0e2c235cd4050928e28b7bc41df
Dmitry <dantreKrt@gmail.com>
2016-04-15 20:01:06 +0500

    Game of life done

--- Task7/client.c
+++ Task7/client.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <netdb.h>
 #include <netinet/in.h>
 
-#include <string.h>
+#define HOST 127.0.0.1
+#define PORT 5001
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) 
+{
    int sockfd, portno, n;
    struct sockaddr_in serv_addr;
    struct hostent *server;
@@ -18,9 +21,7 @@ int main(int argc, char *argv[]) {
       exit(0);
    }
 	
-   portno = atoi(argv[2]);
-   
-   /* Create a socket point */
+   portno = atoi(PORT);   
    sockfd = socket(AF_INET, SOCK_STREAM, 0);
    
    if (sockfd < 0) {
@@ -28,8 +29,7 @@ int main(int argc, char *argv[]) {
       exit(1);
    }
 	
-   server = gethostbyname(argv[1]);
-   
+   server = gethostbyname(HOST);   
    if (server == NULL) {
       fprintf(stderr,"ERROR, no such host\n");
       exit(0);
@@ -38,31 +38,24 @@ int main(int argc, char *argv[]) {
    bzero((char *) &serv_addr, sizeof(serv_addr));
    serv_addr.sin_family = AF_INET;
    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-   serv_addr.sin_port = htons(portno);
+   serv_addr.sin_port = htons(portno);   
    
-   /* Now connect to the server */
    if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
       perror("ERROR connecting");
       exit(1);
    }
    
-   /* Now ask for a message from the user, this message
-      * will be read by server
-   */
-	
    printf("Please enter the message: ");
    bzero(buffer,256);
    fgets(buffer,255,stdin);
-   
-   /* Send message to the server */
+      
    n = write(sockfd, buffer, strlen(buffer));
    
    if (n < 0) {
       perror("ERROR writing to socket");
       exit(1);
    }
-   
-   /* Now read server response */
+      
    bzero(buffer,256);
    n = read(sockfd, buffer, 255);
    
--- Task7/server.c
+++ Task7/server.c
@@ -1,95 +1,169 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <netdb.h>
 #include <netinet/in.h>
 
-#include <string.h>
+#include <unistd.h>
+
+#define PORT 5001
+
+#define HEIGHT 20
+#define WIDTH 40
 
 void doprocessing (int sock);
 
-int main( int argc, char *argv[] ) {
-   int sockfd, newsockfd, portno, clilen;
-   char buffer[256];
-   struct sockaddr_in serv_addr, cli_addr;
-   int n, pid;
-   
-   /* First call to socket() function */
-   sockfd = socket(AF_INET, SOCK_STREAM, 0);
-   
-   if (sockfd < 0) {
-      perror("ERROR opening socket");
-      exit(1);
-   }
-   
-   /* Initialize socket structure */
-   bzero((char *) &serv_addr, sizeof(serv_addr));
-   portno = 5001;
-   
-   serv_addr.sin_family = AF_INET;
-   serv_addr.sin_addr.s_addr = INADDR_ANY;
-   serv_addr.sin_port = htons(portno);
-   
-   /* Now bind the host address using bind() call.*/
-   if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-      perror("ERROR on binding");
-      exit(1);
-   }
-   
-   /* Now start listening for the clients, here
-      * process will go in sleep mode and will wait
-      * for the incoming connection
-   */
-   
-   listen(sockfd,5);
-   clilen = sizeof(cli_addr);
-   
-   while (1) {
-      newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-		
-      if (newsockfd < 0) {
-         perror("ERROR on accept");
-         exit(1);
-      }
-      
-      /* Create child process */
-      pid = fork();
-		
-      if (pid < 0) {
-         perror("ERROR on fork");
-         exit(1);
-      }
-      
-      if (pid == 0) {
-         /* This is the client process */
-         close(sockfd);
-         doprocessing(newsockfd);
-         exit(0);
-      }
-      else {
-         close(newsockfd);
-      }
-		
-   } /* end of while */
+void make_step();
+void print_board();
+
+int count(int i, int j);
+int get_value(int x, int y);
+
+int main( int argc, char *argv[] )
+{
+    int i;    
+    for (i = 0; i < 100; i++) {        
+		print_board();
+        make_step();    
+        sleep(1);        
+    }
+    /*
+    int sockfd, newsockfd, portno;
+    socklen_t clilen;
+    //char buffer[256];
+    struct sockaddr_in serv_addr, cli_addr;
+    int pid; //,n;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("ERROR opening socket");
+        exit(1);
+    }
+
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    portno = PORT;
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        perror("ERROR on binding");
+        exit(1);
+    }
+
+    listen(sockfd, 5);
+    clilen = sizeof(cli_addr);
+
+    while (1) {
+        newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+
+        if (newsockfd < 0) {
+            perror("ERROR on accept");
+            exit(1);
+        }
+        pid = fork();
+        if (pid < 0) {
+            perror("ERROR on fork");
+            exit(1);
+        }
+        if (pid == 0) {
+            close(sockfd);
+            doprocessing(newsockfd);
+            exit(0);
+        }
+        else
+            close(newsockfd);
+    }*/
+}
+/*
+void doprocessing (int sock)
+{
+    int n;
+    char buffer[256];
+    bzero(buffer,256);
+    n = read(sock,buffer,255);
+
+    if (n < 0) {
+        perror("ERROR reading from socket");
+        exit(1);
+    }
+    printf("Here is the message: %s\n",buffer);
+    n = write(sock,"I got your message",18);
+    if (n < 0) {
+        perror("ERROR writing to socket");
+        exit(1);
+    }
+}
+*/
+
+char board[HEIGHT][WIDTH] = { 	    "##*********##***************************",
+                                    "##********#**#***##*****#*******###*****",
+                                    "***********#*#***#*#****#******###******",
+                                    "************#*****#*****#***************",
+                                    "****************************************",
+                                    "****************************************",
+                                    "****************************************",
+                                    "*************************#**************",
+                                    "***********************#*#**************",
+                                    "*************##******##**************##*",
+                                    "************#***#****##**************##*",
+                                    "*##********#*****#***##*****************",
+                                    "*##********#***#*##****#*#**************",
+                                    "***********#*****#*******#**************",
+                                    "************#***#***********************",
+                                    "*************##*************************",
+                                    "****************************************",
+                                    "****************************************",
+                                    "****************************************",
+                                    "****************************************"
+                             };
+
+int count(int i, int j)
+{
+    int x, y, count =0;
+
+    for (x = -1; x <= 1; x++)
+        for (y = -1; y <= 1; y++)
+            if ((x || y) && get_value(x+i,y+j))
+                count++;
+    return count;
 }
 
-void doprocessing (int sock) {
-   int n;
-   char buffer[256];
-   bzero(buffer,256);
-   n = read(sock,buffer,255);
-   
-   if (n < 0) {
-      perror("ERROR reading from socket");
-      exit(1);
-   }
-   
-   printf("Here is the message: %s\n",buffer);
-   n = write(sock,"I got your message",18);
-   
-   if (n < 0) {
-      perror("ERROR writing to socket");
-      exit(1);
-   }
-	
+int get_value(int x, int y)
+{
+    if (x>=0 && x<HEIGHT && y>=0 && y<WIDTH && board[x][y]=='#')
+        return 1;
+    return 0;
+}
+
+void make_step()
+{
+    int	i, j, t;
+    char tmpBoard[HEIGHT][WIDTH];
+
+    for (i = 0; i < HEIGHT; i++)
+        for (j = 0; j < WIDTH; j++)
+        {
+            t = count(i, j);
+            if (t == 2) tmpBoard[i][j] = board[i][j];
+            if (t == 3) tmpBoard[i][j] = '#';
+            if (t < 2) tmpBoard[i][j] = '*';
+            if (t > 3) tmpBoard[i][j] = '*';
+        }
+    for (i = 0; i < HEIGHT; i++)
+        for (j = 0; j < WIDTH; j++)
+            board[i][j] = tmpBoard[i][j];
+}
+
+void print_board()
+{
+	system("clear");
+    int i,j;
+    for (i = 0; i < HEIGHT; i++) {
+        for (j = 0; j < WIDTH; j++)
+            printf("%c", board[i][j]);
+        printf("\n");
+    }
 }
